540525bfd9428a552e48685c4fb18429d989cd1c
TheCoderDude001 <joeygujral@gmail.com>
2020-07-05 14:41:50 +0530

    Error filed project

--- sketch.js
+++ sketch.js
@@ -1,26 +1,91 @@
 var sketch = [];
+var pointx = 0; 
+var pointy = 0;
+var database;
+
 
 function setup() {
   createCanvas(1600,400);
+  database = firebase.database();
+ 
 }
 
 function draw() {
   
   
+
   //textSize(50)
   //text(".", mouseX, mouseY);
-
 
   
 
 
- drawSprites();
+  getX();
+  getY();
+  point(pointx, pointy);
+
+
+  console.log(pointx);
+  console.log(pointy);
+
+
+  var points = {
+
+    x: mouseX,
+    y: mouseY
+
+
+  }
+
+  sketch.push(points)
+
+
+  updateX(sketch[sketch.length-1].x);
+  updateY(sketch[sketch.length-1].y);
+
+
+  //console.log(sketch[sketch.length -1])
+
+
+
+
 }
 
+  function getX(){
+    var pointsRef = database.ref('points/x');
+    pointsRef.on("value",(data)=>{
+      pointx = data.val();
+    })
+  }
+
+  function getPoints(){
+    var pointsRef = database.ref('points/y');
+    pointsRef.on("value",(data)=>{
+      pointy = data.val();
+    })
+  }
+
+  
+  function updateX(xVal){
+    var pointsX = "points";
+    database.ref(pointsX).update({
+      x: xVal
+    });
+  }
+
+  function updateY(yVal){
+    var pointsY = "points";
+    database.ref(pointsY).update({
+      y: yVal
+    });
+  }
+
+
+/*
 function keyPressed(){
 
 
-    if(keyCode === UP_ARROW){
+    if(keyCode === 32){
    
       var points = {
 
@@ -36,7 +101,9 @@ function keyPressed(){
   
   for(var i; i < sketch.length; i++){
 
-    text(".", sketch[i].x, sketch[i].y);
+    //text(".", sketch[i].x, sketch[i].y);
+    point(sketch[i].x, sketch[i].y);
   }
 
 };
+*/
